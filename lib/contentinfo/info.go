c4f658ee0a9a6dbe2d3d8839f258fe473389c480
masato yamauchi <masato.yamauchi@livesense.co.jp>
2016-05-11 15:32:41 +0900

    refact GetContentInfo

--- lib/contentinfo/info.go
+++ lib/contentinfo/info.go
@@ -12,6 +12,56 @@ type ContentInfo struct {
 	Meta         map[string]interface{}
 }
 
+type provider struct {
+	alias string
+	meta  interface{}
+}
+
+func getProviders(c *configure.Conf) []provider {
+	ret := []provider{}
+	for _, p := range c.Providers() {
+		for alias, meta := range convertInterfaceToMap(p) {
+			ret = append(ret, provider{alias, meta})
+		}
+	}
+	return ret
+}
+
+func getContentInfo(urlPath string, p []provider) *ContentInfo {
+	var ret ContentInfo
+	ret.Meta = map[string]interface{}{}
+	index := serachProviderIndex(urlPath, p)
+	if index < 0 {
+		return nil
+	}
+	targetProvider := p[index]
+	for k, v := range convertInterfaceToMap(targetProvider.meta) {
+		switch k {
+		case "src":
+			src := v.(string)
+			path := urlPath[len(targetProvider.alias):]
+			if !strings.HasPrefix(path, "/") {
+				path = "/" + path
+			}
+			ret.ContentPlace = src + path
+		case "type":
+			ret.ContentType = v.(string)
+		default:
+			ret.Meta[k] = v
+		}
+	}
+	return &ret
+}
+
+func serachProviderIndex(urlPath string, p []provider) int {
+	for i, v := range p {
+		if strings.Contains(urlPath, v.alias) {
+			return i
+		}
+	}
+	return -1
+}
+
 func convertInterfaceToMap(i interface{}) map[string]interface{} {
 	if ret, ok := i.(map[string]interface{}); ok {
 		return ret
@@ -20,36 +70,13 @@ func convertInterfaceToMap(i interface{}) map[string]interface{} {
 }
 
 func GetContentInfo(urlPath string, conf *configure.Conf) *ContentInfo {
-	var ret ContentInfo
-	ret.Meta = map[string]interface{}{}
 	if urlPath == "" {
 		return nil
 	}
 	if conf == nil {
 		return nil
 	}
-	for _, buffer := range conf.Providers() {
-		provider := convertInterfaceToMap(buffer)
-		for key, meta := range provider {
-			if strings.HasPrefix(urlPath, key) {
-				for k, info := range convertInterfaceToMap(meta) {
-					switch k {
-					case "src":
-						src := info.(string)
-						path := urlPath[len(key):]
-						if !strings.HasPrefix(path, "/") {
-							path = "/" + path
-						}
-						ret.ContentPlace = src + path
-					case "type":
-						ret.ContentType = info.(string)
-					default:
-						ret.Meta[k] = info
-					}
-				}
-				return &ret
-			}
-		}
-	}
-	return nil
+	providers := getProviders(conf)
+
+	return getContentInfo(urlPath, providers)
 }
